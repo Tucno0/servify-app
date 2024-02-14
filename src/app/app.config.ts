bfb79282593467f8c563fdddee788f7b419748ce
Jhampier <yantucnoperez@gmail.com>
2024-02-14 15:07:06 -0500

    Update app.config.ts with Angular router and HTTP client providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,38 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(
+      //* rutas de la aplicación
+      routes,
+
+      //* para enlazar los parámetros de la URL con los inputs de los componentes
+      withComponentInputBinding(),
+
+
+      //* para animar las transiciones entre vistas
+      withViewTransitions({
+        skipInitialTransition: true, // para que no se ejecute la animación en la primera carga
+        onViewTransitionCreated(transitionInfo) { // para añadir una clase a la vista que se está cargando
+          console.log('onViewTransitionCreated', transitionInfo);
+        },
+      })
+    ),
+
+    //* para importar los providers de otros módulos
+    importProvidersFrom(
+      // HttpClientModule, // para poder hacer peticiones HTTP
+    ),
+
+    //* para poder hacer peticiones HTTP
+    provideHttpClient()
+  ],
 };
